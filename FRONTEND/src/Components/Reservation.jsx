0532130cd3999577a859613821d5de1189db78a1
keshavvijay092 <159714230+keshavvijay092@users.noreply.github.com>
2024-08-06 07:59:49 +0530

    Update Reservation.jsx

--- FRONTEND/src/Components/Reservation.jsx
+++ FRONTEND/src/Components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "https://mern-hotel-backend-bzpz.onrender.com",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
